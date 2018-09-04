fecb5bbfa0b171b6bddfd9fe5462b73091bf1bcf
Kelvin Stott <kstott@neworbit.co.uk>
2018-09-04 12:20:08 +0100

    Corrected syntax for add course button

--- src/ReactPlayground.App/src/components/course/CoursesPage.tsx
+++ src/ReactPlayground.App/src/components/course/CoursesPage.tsx
@@ -6,6 +6,7 @@ import { CourseList } from "./CourseList";
 
 import { Button, Loader, Dimmer } from 'semantic-ui-react';
 import { isRequestActive } from "redux-request-loading";
+import { Link } from "redux-little-router";
 
 export class CoursesPage extends React.Component<any, any> {
     constructor(props: any) {
@@ -19,7 +20,7 @@ export class CoursesPage extends React.Component<any, any> {
                     <Loader />
                 </Dimmer>
                 <h1>Courses</h1>
-                <Button href="/course" primary>Add Course</Button>
+                <Link href="/course"><Button primary>Add Course</Button></Link>
                 <CourseList courses={this.props.courses} />
             </div>
         );
